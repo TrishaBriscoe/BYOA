58405a46fb46ada6649614f1de890502031f82d4
Trisha Briscoe <tbris225@d127.org>
2022-12-13 08:36:52 -0600

    buttony buttons buttons buttons buttons buttons buttons

--- BYOA/ViewController.swift
+++ BYOA/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+  
+        }
+
+    @IBAction func button1(_ sender: UIButton) {
     }
-
-
+    @IBAction func button2(_ sender: UIButton) {
+    }
+    @IBAction func button3(_ sender: UIButton) {
+    }
+    @IBAction func button4(_ sender: UIButton) {
+    }
+    
+    @IBAction func button5(_ sender: UIButton) {
+    }
+    
+    @IBAction func button6(_ sender: UIButton) {
+    }
 }
 
